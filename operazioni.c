c72779b4874aee84988604ae17208a150e4c4790
tonyiovino <antonioiovino2004tony@gmail.com>
2019-05-13 22:27:33 +0000

    Operazioni

--- operazioni.c
+++ operazioni.c
@@ -4,9 +4,9 @@ int main()
 	int numero1, numero2;
 	int somma, differenza, prodotto, quoto, resto;
 
-	printf("Inserisci una cifra ");
+	printf("Inserisci una cifra: ");
 	scanf("%d", &numero1); 
-	printf("Inserisci un'altra cifra ");
+	printf("Inserisci un'altra cifra: ");
 	scanf("%d", &numero2);
 
 	somma = numero1 + numero2;
@@ -19,7 +19,7 @@ int main()
 	printf("%d - %d = %d\n", numero1, numero2, differenza);
 	printf("%d * %d = %d\n", numero1, numero2, prodotto);
 	printf("%d / %d = %d\n", numero1, numero2, quoto);
-	printf("%d % %d = %d\n", numero1, numero2, resto);
+	printf("%d %% %d = %d\n", numero1, numero2, resto);
 
 	return 0;
 }
