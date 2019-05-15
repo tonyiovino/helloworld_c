839d33813ed60fb19aa2c6d180725131f7a8408e
tonyiovino <antonioiovino2004tony@gmail.com>
2019-05-15 21:32:22 +0000

    Cerchio

--- cerchio.c
+++ cerchio.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 int main()
 {
-	int raggio, superficie;
+	int raggio;
 	double pigreco = 3.1415926535;
+	double superficie;
+	double circonferenza;
 
 	printf("Inserisci il raggio: ");
 	scanf("%d", &raggio);
 
 	superficie = pigreco * raggio * raggio;
+	circonferenza = 2.0 * pigreco * raggio;
 
-	printf("La superficie è %d.\n",superficie);
+	printf("La superficie è %f.\n", superficie);
+	printf("La circonferenza è %f.\n", circonferenza);
 
 	return 0;
 }
