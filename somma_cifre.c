de0944aa3accdeff4c34af72ab7796093563d095
tonyiovino <antonioiovino2004tony@gmal.com>
2019-05-22 18:51:59 +0000

    Somma del numero a 3 cifre

---
+++ somma_cifre.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int a1, a2, a3;
+	int b;
+	int min = 0;
+	int max = 999;
+
+	printf("Inserisci un numero di tre cifre.\n");
+	printf("Prima cifra: ");
+	scanf("%d", &a1);
+	printf("Seconda cifra: ");
+	scanf("%d", &a2);
+	printf("Terza cifra: ");
+	scanf("%d", &a3);
+
+	a == a1, a2, a3;
+
+	if (a < min || a > max) {
+		printf("Per favore, inserire un numero di tre cifre.\n");
+} 	else {
+	a = a1 + a2 + a3;
+
+	printf("La somma delle tre cifre è %d\n", a);
+}
+
+	return 0;
+}
