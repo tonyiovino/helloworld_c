86d17b09725622db61e86c432189193144038f4c
tonyiovino <antonioiovino2004tony@gmail.com>
2019-05-23 20:29:41 +0000

    Somma delle cifre

--- somma_cifre.c
+++ somma_cifre.c
@@ -4,21 +4,11 @@ int main()
 {
 	int a;
 	int a1, a2, a3;
-	int b;
-	int min = 0;
-	int max = 999;
 
-	printf("Inserisci un numero di tre cifre.\n");
-	printf("Prima cifra: ");
-	scanf("%d", &a1);
-	printf("Seconda cifra: ");
-	scanf("%d", &a2);
-	printf("Terza cifra: ");
-	scanf("%d", &a3);
+	printf("Inserisci un numero di tre cifre: ");
+	scanf("%d", &a);
 
-	a == a1, a2, a3;
-
-	if (a < min || a > max) {
+	if (a < 100 || a > 999) {
 		printf("Per favore, inserire un numero di tre cifre.\n");
 } 	else {
 	a = a1 + a2 + a3;
